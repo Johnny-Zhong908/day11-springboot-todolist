036cf08df24b60eec27d14968e0fac6c766cf91c
My Name <myEmail@example.com>
2024-12-02 21:43:53 +0800

    feat:complete TodoControllerIntegrationTest

--- src/main/java/com/todo/day11springboottodolist/Day11SpringbootTodolistApplication.java
+++ src/main/java/com/todo/day11springboottodolist/Day11SpringbootTodolistApplication.java
@@ -2,11 +2,14 @@ package com.todo.day11springboottodolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.todo")
 public class Day11SpringbootTodolistApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Day11SpringbootTodolistApplication.class, args);
     }
 
--- src/main/java/com/todo/model/Todo.java
+++ src/main/java/com/todo/model/Todo.java
@@ -19,8 +19,16 @@ public class Todo {
     @Setter
     private boolean done;
 
-
+    public Todo(Integer id,String text,boolean done){
+        this.id = id;
+        this.text = text;
+        this.done = done;
+    }
     public boolean getDone() {
         return done;
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
